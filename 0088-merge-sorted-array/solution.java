2dafaa2646bf7c2b172cb553f3a5053868c81c81
Griffin Charlson <gcharlson25@gmail.com>
2026-04-01 21:16:23 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 43.6 MB (87.50%)

---
+++ 0088-merge-sorted-array/solution.java
@@ -0,0 +1,23 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = m-1;
+        int j = n-1;
+        int k = m+n-1;
+        while(k >= 0) {
+            if(j < 0) {
+                break;
+            }
+            if(i<0 || nums2[j] > nums1[i]){
+                nums1[k] = nums2[j];
+                j--;
+                k--;
+            }
+            else if(nums1[i] >= nums2[j]) {
+                nums1[k] = nums1[i];
+                i--;
+                k--;
+            }
+        }
+
+    }
+}
