eb068e2f990291d25ec3f333d9e7f35752f6b23f
Griffin Charlson <gcharlson25@gmail.com>
2026-04-15 16:39:31 +0000

    Sync LeetCode submission Runtime - 14 ms (31.03%), Memory - 46.2 MB (30.70%)

--- 0125-valid-palindrome/solution.java
+++ 0125-valid-palindrome/solution.java
@@ -1,21 +1,16 @@
 class Solution {
     public boolean isPalindrome(String s) {
         String string = s.toLowerCase();
-        string = string.replaceAll("[^a-zA-Z0-9]", "");
+        string = string.replaceAll("[^a-z0-9]", "");
         int end = string.length()-1;
-        int found = 0;
         for(int i = 0; i<string.length()/2; i++) {
             if(string.charAt(i) != string.charAt(end)) {
-                found = 1;
-                break;
+                return false;
             }
-            else {
-                end--;
-            }
-        }
-        if(found == 1) {
-            return false;
+            end--;
         }
         return true;
+
+
     }
 }
