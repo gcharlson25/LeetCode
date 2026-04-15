7169401f794cb47e47ebfd5be5fd445e427e9eec
Griffin Charlson <gcharlson25@gmail.com>
2026-04-15 16:47:52 +0000

    Sync LeetCode submission Runtime - 2 ms (96.38%), Memory - 48.5 MB (24.22%)

--- 0167-two-sum-ii---input-array-is-sorted/solution.java
+++ 0167-two-sum-ii---input-array-is-sorted/solution.java
@@ -3,20 +3,19 @@ class Solution {
         int[] result = new int[2];
         int front = 0;
         int back = numbers.length-1;
-        while(front < numbers.length && back >= 0) {
+        while(front < back) {
             if(numbers[front] + numbers[back] == target) {
                 result[0] = front+1;
                 result[1] = back+1;
                 break;
-            }
-            else if(numbers[front] + numbers[back] <= target) {
-                front++;
-            }
-            else if(numbers[front] + numbers[back] >= target) {
+            } 
+            else if (numbers[front] + numbers[back] > target){
                 back--;
-            }
+            }  
+            else if (numbers[front] + numbers[back] < target){
+                front++;
+            }              
         }
         return result;
-
     }
 }
