baa90fe493a8d6ede3a6fb38a88885215aa04d30
Griffin Charlson <gcharlson25@gmail.com>
2026-05-14 21:27:21 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 46.4 MB (85.52%)

---
+++ 0138-copy-list-with-random-pointer/solution.java
@@ -0,0 +1,36 @@
+/*
+// Definition for a Node.
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
+*/
+
+class Solution {
+    public Node copyRandomList(Node head) {
+        if(head == null) {
+            return null;
+        }
+        HashMap <Node, Node> map = new HashMap<>();
+        Node cur = head;
+        while(cur != null) {
+            Node copy = new Node(cur.val);
+            map.put(cur, copy);
+            cur = cur.next;
+        }
+        cur = head;
+        while(cur != null) {
+            map.get(cur).next = map.get(cur.next);
+            map.get(cur).random = map.get(cur.random);
+            cur = cur.next;
+        }
+        return map.get(head);
+    }
+}
