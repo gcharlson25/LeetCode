96dcc5c68c722c7a9a528e7b4b529cb38ee940fd
Griffin Charlson <gcharlson25@gmail.com>
2026-05-21 06:12:15 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 46.9 MB (32.22%)

---
+++ 0230-kth-smallest-element-in-a-bst/solution.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    private int count = 0;
+    private int ret = 0;
+    public int kthSmallest(TreeNode root, int k) {
+        inorder(root, k);
+        return ret;
+    }
+    private void inorder(TreeNode root, int k) {
+        if(root == null) {
+            return;
+        }
+        inorder(root.left, k);
+        count++;
+        if(count == k) {
+            ret = root.val;
+        }
+        inorder(root.right, k);
+    }
+}
