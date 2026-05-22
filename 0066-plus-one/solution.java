c4a338cce608a644e3700f972c2e88ff968d508a
Griffin Charlson <gcharlson25@gmail.com>
2026-05-22 19:59:16 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 43.4 MB (41.39%)

--- 0066-plus-one/solution.java
+++ 0066-plus-one/solution.java
@@ -1,22 +1,20 @@
 class Solution {
     public int[] plusOne(int[] digits) {
-        int carry = 0;
-        digits[digits.length - 1]++;
         for(int i = digits.length - 1; i >= 0; i--) {
-            if(carry == 1) {
-                digits[i]++;
-                carry = 0;
+            if(digits[i] + 1 != 10) {
+                digits[i] = digits[i] + 1;
+                break;
             }
-            if(digits[i] == 10) {
+            else {
                 digits[i] = 0;
-                carry = 1;
+                if(i == 0) {
+                    int[] ret = new int[digits.length + 1];
+                    ret[0] = 1;
+                    return ret;
+                }
             }
-        }
-        if(carry == 1) {
-            int[] bigger = new int[digits.length + 1];
-            bigger[0] = 1;
-            return bigger;
         }
         return digits;
+
     }
 }
